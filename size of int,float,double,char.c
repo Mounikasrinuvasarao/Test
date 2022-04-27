b6f024b9c36fea3f9630cb443f81ec010255701c
pentakota mounika <mounikasrinu273@gmail.com>
2022-04-27 19:22:35 +0530

    first commit

---
+++ size of int,float,double,char.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int inttype;
+ float floattype;
+ double doubletype;
+ char chartype;
+ printf("size of int: %ld bytes\n", sizeof(inttype));
+ printf("size of float: %ld bytes\n", sizeof(floattype));
+ printf("size of double: %ld bytes\n", sizeof(doubletype));
+ printf("size of char: %ld bytes\n", sizeof(chartype));
+ return 0;
+}
